7f5237342d216ef0ec3c98575b86eeeff1edd19b
Navneet Shahi <navneetshahi345@gmail.com>
2024-11-12 19:31:46 +0530

    update 7 files

---
+++ src/components/AuthForm.tsx
@@ -0,0 +1,11 @@
+type FormType = "sign-up" | "sign-in";
+
+const AuthForm = ({ type }: { type: FormType }) => {
+  return (
+    <div>
+      <h1>AuthForm</h1>
+    </div>
+  );
+};
+
+export default AuthForm;
